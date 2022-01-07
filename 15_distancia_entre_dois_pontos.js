be606c7d96f81d0c7ceaa52217607665ce6f1b61
Raphael <raphaxd18@gmail.com>
2022-01-07 00:15:35 -0300

    primeiro push oficial

--- 15_distancia_entre_dois_pontos.js
+++ 15_distancia_entre_dois_pontos.js
@@ -1,4 +1,10 @@
-var input = require("fs").readFileSync("stdin", "utf8");
+const input = require("fs").readFileSync("stdin", "utf8");
 
-var [x1, y1] = input.split(" ").map(item => parseFloat(item));
-var [x2, y2] = input.split(" ").map(item => parseFloat(item));
+const valores = input.split("\n");
+
+var [x1, y1] = valores[0].split(" ");
+var [x2, y2] = valores[1].split(" ");
+
+var distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+console.log(distancia.toFixed(4));
---
+++ 16_distancia.js
@@ -0,0 +1,5 @@
+const input = require("fs").readFileSync("stdin", "utf8");
+
+var temp = input * 2;
+
+console.log(temp + " minutos");
