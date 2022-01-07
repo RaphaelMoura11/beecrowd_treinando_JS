2697bb520421a06ea4e1687a859faf2a790d8df1
Raphael <raphaxd18@gmail.com>
2022-01-06 23:30:36 -0300

    teste 3

--- 15_distancia_entre_dois_pontos.js
+++ 15_distancia_entre_dois_pontos.js
@@ -2,5 +2,3 @@ var input = require("fs").readFileSync("stdin", "utf8");
 
 var [x1, y1] = input.split(" ").map(item => parseFloat(item));
 var [x2, y2] = input.split(" ").map(item => parseFloat(item));
-
-
